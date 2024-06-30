636bcd9a3aed63923a07cf7a0af3071b2bc12c14
Alexey <cappuccino13371@gmail.com>
2024-06-30 17:49:49 +0300

    add and update myapp3

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),
-    # path('myapp3/', include('myapp3.urls')),
+    path('myapp3/', include('myapp3.urls')),
     # path('myapp4/', include('myapp4.urls')),
     # path('__debug__/', include("debug_toolbar.urls")),
 ]
