44444c1bfa1e14fff0408ec8b3c7d668d49fb2b6
Alexey <cappuccino13371@gmail.com>
2024-07-01 15:18:48 +0300

    add and update myapp4

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('myapp.urls')),
     path('myapp3/', include('myapp3.urls')),
-    # path('myapp4/', include('myapp4.urls')),
+    path('myapp4/', include('myapp4.urls')),
     # path('__debug__/', include("debug_toolbar.urls")),
 ]
